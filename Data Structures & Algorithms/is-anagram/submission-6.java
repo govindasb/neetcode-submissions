e2406820e41b78bb377d41217c73e52408d618df
govindasb <gbandaru133@gmail.com>
2026-07-02 17:52:57 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+        for(int i=0;i <s.length();i ++){
+            if(map1.containsKey(s.charAt(i))){
+                map1.put(s.charAt(i), map1.get(s.charAt(i))+1);
+            }
+            else{
+                map1.put(s.charAt(i),0);
+            }
+            if(map2.containsKey(t.charAt(i))){
+                map2.put(t.charAt(i), map2.get(t.charAt(i))+1);
+            }
+            else{
+                map2.put(t.charAt(i),0);
+            }
+        }
+        return map1.equals(map2);
+    }
+
+}
